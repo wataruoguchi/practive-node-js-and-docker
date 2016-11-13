f79b15ad1a305c43408ad57ca834cd0453f906ec
wataru <wataru.oguchi.0to1@gmail.com>
2016-11-13 23:01:09 +0000

    Add mondodb

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 var express = require('express');
 var app = express();
+var bodyparser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://' + process.env.MONGO_PORT_27017_TCP_ADDR + ':27017/dockerdemo';
+var db;
 
+MongoClient.connect(url, function (err, database) {
+  console.log("Connected correctly to server");
+  db = database;
+});
+
+app.use(bodyparser.json());
 app.use(express.static('public'));
+
+var insertDocument = function (db, document, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Insert some documents
+  collection.insertOne(document, function (err, result) {
+    callback(err, JSON.stringify(result.ops[0]));
+  });
+};
+
+// eg,  echo '{"name": "PDMLab"}' | curl -d @- http://localhost:3000/hello
+app.post('/hello', function (req, res) {
+  var data = req.body;
+  insertDocument(db, data, function (err, result) {
+    res.status(201).send(result);
+  });
+});
 
 app.get('/hello', function (req, res) {
   res.send('world');
