38b74d28e0b1d9acd61f05cba64b49e86774570e
wataru <wataru.oguchi.0to1@gmail.com>
2016-11-13 00:03:23 +0000

    Add GET '/hello' endpoint It requires re-build, which is not mentioned

--- app.js
+++ app.js
@@ -2,4 +2,9 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
+
+app.get('/hello', function (req, res) {
+  res.send('world');
+});
+
 app.listen(3000);
